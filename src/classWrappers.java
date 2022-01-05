efe9563152f76bd594ebf923d5b1a516955e3fa0
Isaac8702 <isacro31@gmail.com>
2022-01-05 15:44:10 -0300

    arquivo java

---
+++ src/classWrappers.java
@@ -0,0 +1,16 @@
+public class classWrappers {
+
+    public static void main (String[] args) {
+
+        Byte byteW = 340;
+        Short shortW = 3;
+        Integer intW = 3;
+        Long longW = 40L;
+        Float floatW = 40F;
+        Double doubleW = 40D;
+        Character charW = 'C';
+        Boolean booleanW = true;
+
+    }
+
+}
